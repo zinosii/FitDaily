a8a76f0fd8394ff4d47cf17737490aaf1332ba13
zino_macOS <jinho142857@gmail.com>
2023-11-22 01:08:50 +0900

    Update index page

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var backgrounds = [
+        'linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/assets/eximg1.png)',
+        'linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/assets/eximg2.png)',
+        'linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/assets/eximg3.png)',
+        'linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/assets/eximg4.png)',
+        'linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/assets/eximg5.png)',
+    ];
+
+    var currentBackground = 0;
+    $('#prev1').click(function () {
+        currentBackground = (currentBackground + 4) % backgrounds.length;
+        $('#imgs_ver1').css('background-image', backgrounds[currentBackground]);
+        $('#imgs_ver2').css('background-image', backgrounds[currentBackground]);
+        
+    });
+    $('#next1').click(function () {
+        currentBackground = (currentBackground + 1) % backgrounds.length;
+        $('#imgs_ver1').css('background-image', backgrounds[currentBackground]);
+        $('#imgs_ver2').css('background-image', backgrounds[currentBackground]);
+        
+    });
+    $('#prev2').click(function () {
+        currentBackground = (currentBackground + 4) % backgrounds.length;
+        $('#imgs_ver1').css('background-image', backgrounds[currentBackground]);
+        $('#imgs_ver2').css('background-image', backgrounds[currentBackground]);
+        
+    });
+    $('#next2').click(function () {
+        currentBackground = (currentBackground + 1) % backgrounds.length;
+        $('#imgs_ver1').css('background-image', backgrounds[currentBackground]);
+        $('#imgs_ver2').css('background-image', backgrounds[currentBackground]);
+        
+    });
+});
